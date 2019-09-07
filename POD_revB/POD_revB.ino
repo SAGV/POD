a2d1b43086b4259f0b10e73536da3a8dc9f50743
Artem Golovin <agv.c1d@gmail.com>
2019-09-07 23:46:11 +0200

    Changed pomodoro to 45+15 instead of 25+5
     + blink on reset

--- POD_revB/POD_revB.ino
+++ POD_revB/POD_revB.ino
@@ -1,23 +1,36 @@
+// To flash my sw
+// Use this manual to be able to flash attiny85 via arduino IDE https://www.instructables.com/id/How-to-Program-an-Attiny85-From-an-Arduino-Uno/
+// Don't forget to set internal clock to 16Mhz and board to attiny85 in arduino ide -> tools
 #include <avr/sleep.h>
 
-#define workInterv 300000
-#define breakInterv 60000
-#define flashInterv 250
+#define workInterv  33750 //9 * 60 * 1000 / 16 
+#define breakInterv 10875 //3 * 60 * 1000 / 16 
+#define flashInterv 250 / 16 //250 / 16
+#define intermediateDelay 1000 / 16
 
 void setup()
 {
+
   for(int i=0; i<=4; i++)
   {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
 
+  delay(flashInterv);
+  for(int j=0; j<=4; j++)
+    digitalWrite(j, LOW);
+  
+  delay(flashInterv);
+  for(int j=0; j<=4; j++)
+    digitalWrite(j, HIGH);
+
   for(int i=0; i<=4; i++)
   {
     delay(workInterv);
     digitalWrite(i, LOW);
   }
-  delay(1000);
+  delay(intermediateDelay);
 
   for(int i=0; i<=9; i++)
   {
@@ -30,18 +43,14 @@ void setup()
   }
 
   // break
-  for(int i=0; i<=4; i++)
-  {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
-  }
+  for(int i=0; i<=4; i++) digitalWrite(i, HIGH);
 
   for(int i=0; i<=4; i++)
   {
     delay(breakInterv);
     digitalWrite(i, LOW);
   }
-  delay(1000);
+  delay(intermediateDelay);
 
   for(int i=0; i<=9; i++)
   {
